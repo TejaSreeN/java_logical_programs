5cbaf8cffda622f74e54567f3a48510790b02f0d
TejaSreeN <tejasreenallamothu111@gmail.com>
2024-11-07 13:57:30 +0530

    Add files via upload

---
+++ Day 1/Student.java
@@ -0,0 +1,12 @@
+class Student
+{
+public static void main(String args[])
+{
+int stdid=28;
+String std_name="Teja";
+float stdht=55.45f;
+System.out.println("The given student ID number is:"+stdid);
+System.out.println("The given student name is:"+std_name);
+System.out.println("The given student height is:"+stdht);
+}
+}
